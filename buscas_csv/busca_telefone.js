8153fd81542c4a7ba823e9e8e184bed4f9015c26
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-10-29 10:22:43 -0300

    Update

--- buscas_csv/busca_telefone.js
+++ buscas_csv/busca_telefone.js
@@ -2,8 +2,6 @@ const fs = require('fs')
 const bancoCsv = 'database.csv'
 const banco = fs.readFileSync(bancoCsv, "utf-8")
 
-//const regexTelefone = /\d+/g
-
 const regexTelefone = /\(\d+\)\s\d+-\d+/g
 
 const matchTelefone = banco.match(regexTelefone)
